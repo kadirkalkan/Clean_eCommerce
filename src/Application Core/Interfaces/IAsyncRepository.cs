5ce1d8b4507c7c5db874fe2f656af39ab1253ca5
kadirkalkan <kadir.kalkan3475@gmail.com>
2021-10-18 15:53:36 +0300

    AsyncRepository implemented

---
+++ src/Application Core/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Entities;
+// Install-Package Ardalis.Specification
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Core.Interfaces
+{
+    public interface IAsyncRepository<T> where T: BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> ListAllAsync();
+        Task<List<T>> ListAlllAsync(ISpecification<T> spec);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task CountAsync(ISpecification<T> spec);
+        Task FirstAsync(ISpecification<T> spec);
+        Task FirstOrDefaultAsync(ISpecification<T> spec);
+    }
+}
